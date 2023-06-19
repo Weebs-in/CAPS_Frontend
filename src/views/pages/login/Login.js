f1e93743653ea92891cf878babb59d4979c97f9a
ruthtop <mud.cookie@gmail.com>
2023-06-19 23:16:05 +0800

    draft Create Course page

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
         <CRow className="justify-content-center">
           <CCol md={8}>
             <CCardGroup>
-              <CCard className="p-4">
+              <CCard className="p-4" style={{ width: '30%' }}>
                 <CCardBody>
                   <CForm>
                     <h1>Login</h1>
@@ -59,22 +59,22 @@ const Login = () => {
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
+              {/*<CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>*/}
+              {/*  <CCardBody className="text-center">*/}
+              {/*    <div>*/}
+              {/*      <h2>Sign up</h2>*/}
+              {/*      <p>*/}
+              {/*        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod*/}
+              {/*        tempor incididunt ut labore et dolore magna aliqua.*/}
+              {/*      </p>*/}
+              {/*      <Link to="/register">*/}
+              {/*        <CButton color="primary" className="mt-3" active tabIndex={-1}>*/}
+              {/*          Register Now!*/}
+              {/*        </CButton>*/}
+              {/*      </Link>*/}
+              {/*    </div>*/}
+              {/*  </CCardBody>*/}
+              {/*</CCard>*/}
             </CCardGroup>
           </CCol>
         </CRow>
