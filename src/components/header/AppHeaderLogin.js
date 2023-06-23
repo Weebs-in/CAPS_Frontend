3557882400d4c4df67bc9258737b1e5b14798619
ruthtop <mud.cookie@gmail.com>
2023-06-23 12:30:08 +0800

    Changed top-right dropdown header to log in and log out buttons (log out is not appropriately linked yet)

--- src/components/header/AppHeaderLogin.js
+++ src/components/header/AppHeaderLogin.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import {
     CAvatar,
-    CBadge,
+    CBadge, CButton,
     CDropdown,
     CDropdownDivider,
     CDropdownHeader,
@@ -18,26 +18,9 @@ import CIcon from '@coreui/icons-react'
 
 const AppHeaderLogin = () => {
     return (
-        <CDropdown variant="nav-item">
-            <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
-                <CNavItem>Log In</CNavItem>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-                <CDropdownHeader className="bg-light fw-semibold py-2">Login Type</CDropdownHeader>
-                <CDropdownItem to="/login/admin" component={NavLink}>
-                    <CIcon icon={cilUser} className="me-2" />
-                    Administrator
-                </CDropdownItem>
-                <CDropdownItem to="/login/lecturer" component={NavLink}>
-                    <CIcon icon={cilUser} className="me-2" />
-                    Lecturer
-                </CDropdownItem>
-                <CDropdownItem to="/login/student" component={NavLink}>
-                    <CIcon icon={cilUser} className="me-2" />
-                    Student
-                </CDropdownItem>
-            </CDropdownMenu>
-        </CDropdown>
+        <CButton color="secondary" size="lg" to="/login/" component={NavLink}>
+            <CIcon icon={cilUser} className="me-2" />Log In
+        </CButton>
     )
 }
 
